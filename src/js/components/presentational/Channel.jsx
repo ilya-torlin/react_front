f96ad6a4e3df34001f5842a526312c615fc0c0a8
Ilya Torlin <ilya.torlin@gmail.com>
2018-09-16 11:16:50 +0500

    добавил анимацию для каналов

--- src/js/components/presentational/Channel.jsx
+++ src/js/components/presentational/Channel.jsx
@@ -2,29 +2,44 @@ import React, { Component } from 'react';
 import ProgrammList from './ProgramList';
 
 class Channel extends Component{
+    state = {
+        isVisible: false
+    };
+
+
+    handleVisibleFullList = () => {
+        this.setState({ isVisible: !this.state.isVisible});
+    }
+
+
     render() {
         return (
             <div className="col-md-4 col-sm-6 col-xs-12">
-                <div className="tv-program-tile js-tv-program-tile p-2">
+                <div className={"tv-program-tile js-tv-program-tile p-2 " + (this.state.isVisible ? "state-opened" : "")} >
                     <div className="tv-program-tile__inner-wrap js-tv-program-tile__inner-wrap">
                         <div className="tv-program-tile__header js-tv-program-tile__header">
                             <div className="tv-program-tile__header-image">
                                 <img src="//epg.domru.ru/chlogo/1474886439.png"/>
                             </div>
-                            <div className="tv-program-tile__header-title js-tv-program-tile__header-title"><a
-                                className="tv-program-tile__header-title-name" href="#">{this.props.channel.name}</a>
+                            <div onClick={ this.handleVisibleFullList }
+                                className="tv-program-tile__header-title js-tv-program-tile__header-title">
+                                <a
+                                className="tv-program-tile__header-title-name" href="#">
+                                    {this.props.channel.name}
+                                </a>
                                 <div className="tv-program-tile__header-title-show-name">Восьмое чувство [16+]</div>
                                 <div className="tv-program-tile__header-title-show-time">13:00 - 14:00</div>
                             </div>
                         </div>
                          <ProgrammList programms={this.props.programms}/>
-                        <div
+                        <div onClick={ this.handleVisibleFullList }
                             className="tv-program-tile__link-drop tv-program-tile__link-drop_close js-tv-program-tile__link-drop"
                             data-action="close">Свернуть
                         </div>
                     </div>
                     <div className="tv-program-tile__link-drop-wrap">
-                        <div className="tv-program-tile__link-drop js-tv-program-tile__link-drop"
+                        <div onClick={ this.handleVisibleFullList }
+                             className="tv-program-tile__link-drop js-tv-program-tile__link-drop"
                              data-action="open">Развернуть
                         </div>
                     </div>
--- src/js/components/presentational/ChannelList.jsx
+++ src/js/components/presentational/ChannelList.jsx
@@ -4,10 +4,8 @@ import Channel from './Channel';
 class ChannelList extends Component{
     constructor(props) {
         super(props);
-
-        this.state = {};
-
     }
+
     render() {
         console.log(this.props);
         return (
